e8dd42f136c89664ada6c97b7e57dd0a3ad5a4f3
meilando pringgondani <meilando.pringgondani@students.amikom.ac.id>
2023-03-21 11:21:50 +0700

    First Commit

---
+++ Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    public class TaxiOne
+    {
+        public string DriverName { get; set; }
+        public Boolean OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("\n Taxi Info \n");
+            Console.WriteLine("Nama Driver: {0}", DriverName);
+
+            if (OnDuty == true)
+            {
+                Console.WriteLine("Kerja : Iya", OnDuty);
+            }
+            else
+            {
+                Console.WriteLine("Kerja : Tidak", OnDuty);
+            }
+            Console.WriteLine("Nomor Pelanggan : {0}", NumPassenger);
+        }
+        public void PickupPassanger()
+        {
+            Console.WriteLine("\n {0} Sedang Menaikkan Penumpang", DriverName);
+        }
+        public void DropoffPassanger()
+        {
+            Console.WriteLine("\n {0} Sedang Menurunkan Penumpang", DriverName);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TaxiOne taxi = new TaxiOne();
+
+            taxi.DriverName = "Ipul";
+            taxi.OnDuty = false;
+            taxi.NumPassenger = 82739182;
+
+            taxi.TaxiInfo();
+            taxi.PickupPassanger();
+            taxi.DropoffPassanger();
+        }
+    }
+}
